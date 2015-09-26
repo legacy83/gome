24e563cfeb856ace622ceccb6539a9b205742d15
Thiago Senna <trsenna@gmail.com>
2015-09-26 16:33:30 -0300

    removed api and www

--- api/config/aliases.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Yii::setAlias( 'api', dirname( __DIR__ ) );
-Yii::setAlias( 'console', dirname( dirname( __DIR__ ) ) . '/console' );
--- api/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mazoo',
-    'username' => 'mazoo',
-    'password' => 'mazoo@secret',
-    'charset' => 'utf8',
-];
