f0cfc94480f2dcca040e3b01871cc3a9b9c94aa5
Thiago Senna <trsenna@gmail.com>
2015-09-26 16:34:50 -0300

    fork www from yuba

---
+++ www/_vagrant/cli/post-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# -- Composer Asset Plugin
+# =========================================
+composer --no-interaction global require "fxp/composer-asset-plugin:1.0.0"
+
+# -- App Install/Update
+# =========================================
+cd /vagrant/ && composer --no-interaction install
+cd /vagrant/ && composer --no-interaction update
