c07d4eae4aef979e675956bd72856a6bcc01368d
Thiago Senna <trsenna@gmail.com>
2015-10-01 00:08:51 -0300

    minor organization

--- www/web/index.php
+++ www/web/index.php
@@ -9,7 +9,7 @@ require( __DIR__ . '/../../shared/config/bootstrap.php' );
 
 $config = yii\helpers\ArrayHelper::merge(
     require( __DIR__ . '/../../shared/config/main.php' ),
-    require( __DIR__ . '/../config/web.php' )
+    require( __DIR__ . '/../config/main.php' )
 );
 
 ( new yii\web\Application( $config ) )->run();
