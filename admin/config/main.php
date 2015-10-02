e8a77515869fdcc79499d22ac1f38e66ff11bb35
Thiago Senna <trsenna@gmail.com>
2015-10-01 21:40:22 -0300

    saving progress

---
+++ admin/config/main.php
@@ -0,0 +1,75 @@
+<?php
+
+$config = [
+
+    'id' => 'back',
+    'basePath' => dirname( __DIR__ ),
+    'controllerNamespace' => 'back\\controllers',
+    'bootstrap' => [ 'log' ],
+
+    /*
+     * components
+     */
+
+    'components' => [
+
+        /*
+         * components/request
+         */
+
+        'request' => [
+
+            /*
+             * Insert a secret key in the following
+             * This is required by cookie validation
+             */
+
+            'cookieValidationKey' => 'W8p?(G4k32p@S5M',
+
+        ],
+
+        /*
+         * components/cache
+         */
+
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        /*
+         * components/errorHandler
+         */
+
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        /*
+         * components/log
+         */
+
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => [ 'error', 'warning' ],
+                ],
+            ],
+        ],
+
+    ],
+
+    /*
+     * modules
+     */
+
+    'modules' => [
+        'scaffold' => [
+            'class' => 'scaffold\Module',
+        ],
+    ],
+
+];
+
+return $config;
