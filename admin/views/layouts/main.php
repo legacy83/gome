3da0604cad85947a145b23887fe6f7ab48ecac73
Thiago Senna <trsenna@gmail.com>
2015-10-02 00:24:56 -0300

    saving progress

--- admin/views/layouts/main.php
+++ admin/views/layouts/main.php
@@ -21,41 +21,20 @@ $brandIcon = "<i class=\"glyphicon glyphicon-wrench\"></i>&nbsp;";
 \yii\bootstrap\NavBar::begin( [
     'brandLabel' => "{$brandIcon}<span>{$brandLabel}</span>",
     'options' => [ 'class' => 'navbar-default navbar-fixed-top', ],
-    'innerContainerOptions' => [ 'class' => 'container-fluid' ],
 ] );
+
+echo \yii\bootstrap\Nav::widget( [
+    'options' => [ 'class' => 'navbar-nav navbar-right' ],
+    'items' => [
+        [ 'label' => '@todo', 'url' => [ '/scaffold/to-do' ] ],
+        [ 'label' => '@foo', 'url' => [ '/scaffold/foo' ] ],
+    ],
+] );
+
+\yii\bootstrap\NavBar::end();
 ?>
 
-<?php \yii\bootstrap\NavBar::end(); ?>
-
-<nav class="navbar navbar-inverse navbar-fixed-top hidden" role="navigation">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand brand" href="<?= Yii::$app->homeUrl; ?>">
-                <i class="glyphicon glyphicon-wrench"></i>
-                <span>Gome Dashboard</span>
-            </a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="<?= Yii::$app->homeUrl; ?>">Home</a></li>
-                <li><a href="<?= \yii\helpers\Url::toRoute( [ '/scaffold/to-do' ] ); ?>">About</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="<?= \yii\helpers\Url::toRoute( [ '/scaffold/to-do' ] ); ?>">@todo</a></li>
-                <li><a href="<?= \yii\helpers\Url::toRoute( [ '/scaffold/foo' ] ); ?>">@foo</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
 <div class="container">
-
-    <?= $this->render( 'partials/flash-messages.php' ); ?>
 
     <?= $content; ?>
 
