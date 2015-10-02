369011b3b60a646637fbd6d36c96d9d02afafde1
Thiago Senna <trsenna@gmail.com>
2015-10-02 02:11:37 -0300

    saving progress

--- admin/components/bootstrap/DashboardTopNavBar.php
+++ admin/components/bootstrap/DashboardTopNavBar.php
@@ -3,19 +3,37 @@
 namespace back\components\bootstrap;
 
 use yii\base\Widget;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
 
 class DashboardTopNavBar extends Widget
 {
-    public function init()
-    {
-        parent::init();
-        DashboardNavBar::begin();
-    }
+    public $brandLabel = 'Gome Dashboard';
 
     public function run()
     {
-        echo DashboardNav::widget();
-        DashboardNavBar::end();
-        parent::run();
+        $brandLabel = implode( '&nbsp;', [
+            '<i class="glyphicon glyphicon-wrench"></i>',
+            "<span>{$this->brandLabel}</span>",
+        ] );
+
+        NavBar::begin( [
+            'brandLabel' => $brandLabel,
+            'options' => [
+                'class' => 'navbar-default navbar-fixed-top',
+            ],
+        ] );
+
+        echo Nav::widget( [
+            'items' => [
+                [ 'label' => '@todo', 'url' => [ '/scaffold/to-do' ] ],
+                [ 'label' => '@foo', 'url' => [ '/scaffold/foo' ] ],
+            ],
+            'options' => [
+                'class' => 'navbar-nav navbar-right',
+            ],
+        ] );
+
+        NavBar::end();
     }
 }
