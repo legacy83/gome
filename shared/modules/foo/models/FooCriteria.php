587e154b7c6205a7a80a0403f623127996825fbb
Thiago Senna <trsenna@gmail.com>
2015-10-05 01:00:03 -0300

    saving progress

---
+++ shared/modules/foo/models/FooCriteria.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace foo\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+class FooSearch extends Foo
+{
+    public function rules()
+    {
+        return [
+            [ [ 'id' ], 'integer' ],
+            [ [ 'one', 'two', 'three' ], 'safe' ],
+        ];
+    }
+
+    public function scenarios()
+    {
+        return Model::scenarios();
+    }
+    
+    public function search( $params )
+    {
+        $query = Foo::find();
+
+        $dataProvider = new ActiveDataProvider( [
+            'query' => $query,
+        ] );
+
+        $this->load( $params );
+
+        if ( !$this->validate() ) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere( [
+            'id' => $this->id,
+        ] );
+
+        $query->andFilterWhere( [ 'like', 'one', $this->one ] )
+            ->andFilterWhere( [ 'like', 'two', $this->two ] )
+            ->andFilterWhere( [ 'like', 'three', $this->three ] );
+
+        return $dataProvider;
+    }
+}
